ac8f51d39902b16d0b8358d9aa0605d945a264eb
Giải Toán <trangbachhuyen@gmail.com>
2026-01-02 13:54:08 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   return {
-    base: '/TaoDeKiemTra/',
+    base: '/radekiemtra/',
     server: {
       port: 3000,
       host: '0.0.0.0',
